c1aef8ba62a2057c2236419ed4ee9ca99a3e8b8d
SProga <shane.306@hotmail.com>
2021-03-27 20:33:57 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,147 @@
+const imgArr = ["grapes.png", "orange.png", "real-food.png"];
+let sameItem = [];
+let isPair = "";
+
+const rand = () => {
+  return Math.floor(Math.random() * 5 + 1);
+};
+let board_location = [];
+let arr = [];
+while (arr.length < 6) {
+  let r = Math.floor(Math.random() * 6);
+  if (arr.indexOf(r) === -1) arr.push(r);
+}
+
+let counter = 0;
+for (let i = 0; i < imgArr.length; i++) {
+  for (let k = 0; k < 2; k++) {
+    board_location.push([imgArr[i], arr[counter]]);
+    counter++;
+  }
+}
+
+//Sounds
+
+var sound = new Howl({
+  src: ["sounds/success2.mp3"],
+  volume: 0.2,
+});
+var click = new Howl({
+  src: ["sounds/click.mp3"],
+  volume: 0.2,
+});
+function populateBoard() {
+  let game_board = document.querySelector(".game-board");
+  for (let i = 0; i < 6; i++) {
+    let newDiv = document.createElement("button");
+    newDiv.classList.add("card");
+    let cardfront = document.createElement("div");
+    cardfront.classList.add("card--front");
+    let cardback = document.createElement("div");
+    cardback.classList.add("card--back");
+    let img_front = document.createElement("IMG");
+    img_front.src = "imgs/bg2.jpg";
+    img_front.classList.add("item-front");
+    let img_back = document.createElement("IMG");
+    img_back.classList.add("item-back");
+    cardfront.appendChild(img_front);
+    cardback.appendChild(img_back);
+    newDiv.appendChild(cardfront);
+    newDiv.appendChild(cardback);
+    game_board.appendChild(newDiv);
+  }
+}
+
+function populateImages(board) {
+  const img_back = document.querySelectorAll(".item-back");
+  for (let i = 0; i < 6; i++) {
+    for (let k = 0; k < 2; k++) {
+      card_location = board[i][1];
+      img_back[card_location].src = `imgs/${board[i][0]}`;
+    }
+  }
+}
+
+const compare = (arr) => {
+  let p = 0;
+  let n = 1;
+  if (arr[p].lastChild.src !== arr[n].lastChild.src) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+populateBoard();
+populateImages(board_location);
+
+const card = document.querySelectorAll(".card");
+const card_front = document.querySelectorAll(".card--front");
+const card_back = document.querySelectorAll(".card--back");
+let count = 0;
+
+for (let i = 0; i < card.length; i++) {
+  card[i].addEventListener("click", (e) => {
+    click.play();
+    card[i].disabled = true;
+    card_front[i].classList.add("card--front_active");
+    card_back[i].classList.add("card--back_active");
+    count++;
+
+    if (count == 2) {
+      card.forEach((c) => (c.disabled = true));
+    }
+
+    let item = "";
+    item = card_back[i];
+    sameItem.push(item);
+
+    console.log(sameItem);
+
+    if (sameItem.length == 2) {
+      if (compare(sameItem)) {
+        sameItem.forEach((card) => {
+          card.isPair = true;
+        });
+        setTimeout(() => {
+          sound.play();
+        }, 200);
+
+        reset(sameItem, card);
+      } else {
+        sameItem.forEach((card) => {
+          if (card.isPair != true) {
+            card.isPair = false;
+          }
+        });
+
+        reset(sameItem, card);
+      }
+    }
+  });
+}
+
+function reset(card, boardItems) {
+  sameItem = [];
+  count = 0;
+  setTimeout(() => {
+    card.forEach((c) => {
+      if (
+        c.isPair != true &&
+        c.previousSibling.classList.contains("card--front_active")
+      ) {
+        c.previousSibling.classList.remove("card--front_active");
+      }
+      if (c.isPair != true && c.classList.contains("card--back_active")) {
+        c.classList.remove("card--back_active");
+      }
+    });
+    boardItems.forEach((item) => {
+      if (!item.lastChild.classList.contains("card--back_active")) {
+        item.disabled = false;
+      }
+    });
+  }, 1500);
+
+  console.log("currently resetting");
+}
